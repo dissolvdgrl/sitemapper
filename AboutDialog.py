57b4084267126a5bca7bb3daa5d53bf262c12477
dissolvdgrl <c@chilldsgn.com>
2025-07-02 07:29:22 +0200

    basic UI updates

--- AboutDialog.py
+++ AboutDialog.py
@@ -7,12 +7,14 @@ class AboutDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__()
 
-        self.resize(450, 200)
+        self.resize(450, 275)
         self.setWindowTitle("About Sitemapper")
 
         heading = QLabel("<span style='font:bold;font-size:20px;'>About Sitemapper</span>")
         version = QLabel("v1.0")
         developed_by = QLabel("Developed by Christie Hill")
+        blurb1 = QLabel("Sitemapper is a small desktop application that generates XML sitemaps for your websites.")
+        blurb2 = QLabel("I built it to be easy to use and can run on multiple operating systems. ")
         found_bug = QLabel("<span style='font:bold;font-size:16px;'>Found a bug?</span>")
         contact_me = QLabel("Contact me by visiting my website: <a href='https://chilldsgn.com'>chilldsgn.com</a>")
         contact_me.setOpenExternalLinks(True)
@@ -23,6 +25,9 @@ class AboutDialog(QDialog):
 
         layout_vertical.addWidget(heading)
         layout_vertical.addWidget(version)
+        layout_vertical.addWidget(blurb1)
+        layout_vertical.addWidget(blurb2)
+        layout_vertical.addStretch(1)
         layout_vertical.addWidget(developed_by)
         layout_vertical.addWidget(github)
         layout_vertical.addStretch(1)
